1b716b1642d3e98fdea1dce04d889f2342db71d2
Joshua Shifrin <joshua.shifrin@autotrader.com>
2015-12-07 00:44:06 -0500

    views for showing books and adding books

--- views/js/index.js
+++ views/js/index.js
@@ -8,4 +8,33 @@
             $scope.books = books.data;
         });
     });
+    angular.module('bookform', [])
+        .controller('BookFormController', ['$scope', '$http',
+                 function ($scope, $http) {
+                $scope.successMessage = '',
+                    $scope.data = {
+                        title: 'Book Title',
+                        author: 'Book Author',
+                        pages: 0,
+                        published: 'yyyy-dd-MM',
+                        category: 'Book Category',
+                        summary: ''
+                    },
+                    $scope.addBook = function () {
+                        $http.post('/book', $scope.data).then(function (response) {
+                            $scope.successMessage = 'Your Book was Successfully Added';
+                            $scope.data = {
+                                title: 'Book Title',
+                                author: 'Book Author',
+                                pages: 0,
+                                published: 'yyyy-dd-MM',
+                                category: 'Book Category',
+                                summary: ''
+                            };
+                            console.log('Successful');
+                        }, function (error) {
+                            console.error(error);
+                        });
+                    };
+    }]);
 }());
